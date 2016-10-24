4101be497ae012cef07c60a5df35d72fd7a34154
FuzzyLitchi <blemille@hotmail.com>
2016-10-24 23:29:13 +0200

    add a broken joystick

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var game = new Phaser.Game(1300, 700, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var radius = 100;
+var radius_squared;
+var joy_x = 1300-120;
+var joy_y = 700-120;
 
 function preload() {
 
@@ -6,23 +10,47 @@ function preload() {
 
 function create() {
 
+  var style = { font: "65px Arial", fill: "#52bace", align: "center" };
+  text = game.add.text(game.world.centerX, 100, "decoding", style);
+  text.anchor.set(0.5);
+  text.text = "a"
+
+  radius_squared = radius * radius
+
   game.input.addPointer();
-  game.input.addPointer();
-  rect1 =  new Phaser.Rectangle(100, 100, 100, 100)
-  rect2 =  new Phaser.Rectangle(100, 100, 100, 100)
+  rect =  new Phaser.Rectangle(100, 100, 10, 10)
+
 }
 
 function update() {
 
   if (game.input.pointer1.isDown) {
-    rect1.x = game.input.pointer1.x*2
-    rect1.y = game.input.pointer1.y
-  }
-  if (game.input.pointer2.isDown) {
-    rect2.x = game.input.pointer2.x
-    rect2.y = game.input.pointer2.y
+
+    var temp_dist_squared = dist_squared(game.input.pointer1.x, game.input.pointer1.y, joy_x, joy_y);
+    if (temp_dist_squared<radius_squared) {
+
+      rect.x = game.input.pointer1.x
+      rect.y = game.input.pointer1.y
+
+    }else{
+
+      var dist = Math.sqrt(temp_dist_squared);
+      rect.x = radius * (game.input.pointer1.x - joy_x) / dist + joy_x;
+      rect.y = radius * (game.input.pointer1.y - joy_y) / dist + joy_y;
+
+    }
   }
 
-  game.debug.geom(rect1,'#0fffff');
-  game.debug.geom(rect2,'#0fffff');
+  text.text = "" + dist_squared(game.input.pointer1.x, game.input.pointer1.y, joy_x, joy_y)<radius_squared
+
+  game.debug.geom(rect,'#0fffff');
+
+}
+
+function dist_squared(x1, y1, x2, y2) {
+
+  var x = x2 - x1
+  var y = y2 - y1
+  return x*x+y*y
+
 }
